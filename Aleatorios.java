2c52f6345eb5561743f30c928067a8903523a07b
OmarFB-DAM <OmarFerreraBarroso@cipcampus.com>
2024-03-02 14:41:56 +0000

    Código corregido y comentado con la corrección

--- Aleatorios.java
+++ Aleatorios.java
@@ -8,7 +8,7 @@ public class Aleatorios {
 		int elementos = Integer.parseInt(JOptionPane.showInputDialog("Teclea un numero + de uno=>"));
 		int num_aleat[]=new int[elementos];
 		for (int i = 0; i < num_aleat.length; i ++) {
-			num_aleat[i] = (int)(Math.random()*100);
+			num_aleat[i] = (int)(Math.random()*100);//el paréntesis es necesario para que primero se multiplique el número aleatorio y luego se guarde como entero
 		}
 		for (int elem: num_aleat) {
 			System.out.println(elem);
